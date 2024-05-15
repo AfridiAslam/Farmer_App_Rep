7e1664b77e1ceab64d8e358c9b2368cd33f1feac
AfridiAslam <afridiertugrul96@gmail.com>
2024-05-15 12:05:44 +0530

    CLS-7:Added Credentials Security Module

---
+++ SecurityManagement.java
@@ -0,0 +1,20 @@
+public class SecirutyManagement{
+
+
+
+ public String securePayment(){
+
+      //logic here
+     }
+
+public String secureLogin(){
+
+        //logic here
+}
+
+public String secureAccountCredentila(){
+
+
+ //logic here
+}
+}
